1f63ea306ab5928fec5bfd0c1318706469de2f7b
jordan <jharrison1@qa.com>
2022-10-06 15:17:36 +0100

    added middleware example

--- index.js
+++ index.js
@@ -4,18 +4,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const ducks = [];
+const ducks = [{
+    name: "Daffy",
+    colour: "black"
+}];
 
 app.use(bodyParser.json());
+
+// app.use - adds a piece of middleware to the chain
+app.use((req, res, next) => {
+    console.log("Request recevied at", new Date());
+   return next(); // calls the next func
+})
+
+// IS the next function
+app.use((req, res, next) => {
+    console.log("I just exist to be an example");
+    return next();
+})
 
 app.get("/hello", (req, res) => {
     res.send("Hello, World!")
 });
 
-app.get("/getAllDucks", (req, res) => {});
+app.get("/getAllDucks", (req, res) => res.send(ducks));
 
 
-app.get("/getDuck/:id", (req, res) => {});
+app.get("/getDuck/:id", (req, res) => res.send(ducks[req.params.id]));
+
+const deleteMiddleware = (req, res, next) => {
+    console.log("You're trying to DELETE A DUCK? YOU MONSTER!!!");
+    next();
+}
 
 app.post("/createDuck", (req, res) => {
     ducks.push(req.body);
@@ -25,13 +45,13 @@ app.post("/createDuck", (req, res) => {
 app.patch("/updateDuck/:id", (req, res) => {
     console.log("ID:", req.params.id);
     console.log("QUERY:", req.query);
-
     res.send();
 })
 
-app.delete("/removeDuck/:id", (req, res) => {
+app.delete("/removeDuck/:id", deleteMiddleware, (req, res) => {
     console.log("ID:", req.params.id);
-    res.send();
+
+    res.send(ducks.splice(req.params.id));
 });
 
 const server = app.listen(4494, () => console.log(`Server successfully started on port ${server.address().port}`));
