defc7e0662d6886e3012fc562d906d4011a64f09
jordan <jharrison1@qa.com>
2022-10-11 09:38:56 +0100

    added async await

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/tdp_db", {
     useNewUrlParser: true
-});
+}).then(() => console.log("Connected to MongoDB")).catch(err => console.error(err));
 
 // makes a new Schema
 const duckSchema = new mongoose.Schema({
--- routes/ducks.js
+++ routes/ducks.js
@@ -1,36 +1,39 @@
 const router = require("express").Router();
 const { duckModel } = require("../db");
 
-// const ducks = [{
-//     name: "Daffy",
-//     colour: "black"
-// }];
-
 router.get("/getAllDucks", (req, res) => duckModel.find({}).then(results => res.send(results)).catch(err => next(err)));
 
 
 router.get("/getDuck/:id", (req, res, next) => {
     const {id} = req.params;
-    if (!ducks[id]) return next("No duck there");
-    res.send(ducks[id])
+    duckModel.findById(id)
 });
+
+router.post("/createDuck", async (req, res, next) => {
+    if (!req.body.name) return next({ status: 400, message: "Missing name"})
+    try { // attempts to create the doc
+        const result = await duckModel.create(req.body);
+        res.status(201).send(result);
+    } catch(err) { // catches any errors
+        return next(err);
+    }
+});
+
+
+router.patch("/updateDuck/:id", async (req, res, next) => { 
+    try {
+        await duckModel.findByIdAndUpdate(req.params.id, req.query)
+        const newDuck = await duckModel.findById(req.params.id);
+        res.send(newDuck);
+    } catch(err) {
+        return next(err);
+    }
+})
 
 const deleteMiddleware = (req, res, next) => {
     console.log("You're trying to DELETE A DUCK? YOU MONSTER!!!");
     next();
 }
-
-router.post("/createDuck", (req, res, next) => {
-    if (!req.body.name) return next({ status: 400, message: "Missing name"})
-    duckModel.create(req.body).then(result => res.status(201).send(result)).catch(err => next(err));
-});
-
-
-router.patch("/updateDuck/:id", (req, res) => {
-    console.log("ID:", req.params.id);
-    console.log("QUERY:", req.query);
-    res.send();
-})
 
 router.delete("/removeDuck/:id", deleteMiddleware, (req, res, next) => {
     const {id}  = req.params;
